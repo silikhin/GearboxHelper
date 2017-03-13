1bd6cf8c4ced4fade43ffbbd52eb24fddbe7c5e2
Borys Silikhin <silihin@gmail.com>
2017-03-13 09:09:20 +0200

    change java file for ActivityTransmission (not finished)

--- app/src/main/java/com/dukatoil/transmbyvehicle/ActivityTransmission.java
+++ app/src/main/java/com/dukatoil/transmbyvehicle/ActivityTransmission.java
@@ -3,26 +3,28 @@ package com.dukatoil.transmbyvehicle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ActivityTransmission extends AppCompatActivity {
 
-    TextView tvTransmission, tvOil, tvCapacity, tvFilterGasket, tvNotes;
+    TextView tvTransmission;
+    ListView lvAbout;
     String[] components;
     String extra; // extra from ActivityVariants (like "5hp19")
     String res_transmission; //resource array name
     int connector; // resource Id for String array
 
+//    TODO: change to multiline list
+//    TODO: add ImageView for Transmission
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transmission);
 
         tvTransmission = (TextView) findViewById(R.id.tvTransmission);
-        tvOil = (TextView)findViewById(R.id.tvOil);
-        tvCapacity = (TextView)findViewById(R.id.tvCapacity);
-        tvFilterGasket = (TextView)findViewById(R.id.tvFilterGasket);
-        tvNotes = (TextView)findViewById(R.id.tvNotes);
+        lvAbout = (ListView) findViewById(R.id.lvAbout);
 
         Intent intent = getIntent();
         extra = intent.getStringExtra("transmission");
@@ -31,9 +33,8 @@ public class ActivityTransmission extends AppCompatActivity {
         connector = getResources().getIdentifier(res_transmission, "array", getPackageName());
         components = getResources().getStringArray(connector);
 
-        tvOil.setText(components[0]);
-        tvCapacity.setText(components[1]);
-        tvFilterGasket.setText(components[2]);
-        tvNotes.setText(components[3]);
+//        TODO: change the resource to item_activity_transmission
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item_transmission_variants, components);
+        lvAbout.setAdapter(adapter);
     }
 }
