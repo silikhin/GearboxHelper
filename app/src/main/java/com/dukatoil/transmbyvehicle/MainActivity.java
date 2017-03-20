9d2af43864491811bd4b28d103f873112aaabb6d
Borys Silikhin <silihin@gmail.com>
2017-03-20 07:41:49 +0200

    changed textcolor after clicking tv1, tv2, tv3

--- app/src/main/java/com/dukatoil/transmbyvehicle/MainActivity.java
+++ app/src/main/java/com/dukatoil/transmbyvehicle/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     String[] cars_list, models_list, versions_list;
     String model_selected;
     final Context firstActivity = this;
-    LinearLayout.LayoutParams params_show = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    LinearLayout.LayoutParams params_show = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     LinearLayout.LayoutParams params_hide = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1);
 
     @Override
@@ -145,6 +145,8 @@ public class MainActivity extends AppCompatActivity {
                 tv1.setText(R.string.text_view_1);
                 tv2.setText(R.string.text_view_2);
                 tv3.setText(R.string.text_view_3);
+                tv2.setTextColor(getResources().getColor(R.color.textColor54a));
+                tv3.setTextColor(getResources().getColor(R.color.textColor54a));
                 llModels.setLayoutParams(params_hide);
                 llCars.setLayoutParams(params_show);
                 llPrimary1.setBackgroundColor(getResources().getColor(R.color.colorLayout100));
@@ -163,6 +165,8 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     tv2.setText(R.string.text_view_2);
                     tv3.setText(R.string.text_view_3);
+                    tv2.setTextColor(getResources().getColor(R.color.textColor54a));
+                    tv3.setTextColor(getResources().getColor(R.color.textColor54a));
                     llCars.setLayoutParams(params_hide);
                     llVersions.setLayoutParams(params_hide);
                     llModels.setLayoutParams(params_show);
@@ -182,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(firstActivity, "Спочатку виберіть модель автомобіля", Toast.LENGTH_SHORT).show();
                 } else {
                     tv3.setText(R.string.text_view_3);
+                    tv3.setTextColor(getResources().getColor(R.color.textColor54a));
                     llVersions.setLayoutParams(params_show);
                     llPrimary3.setBackgroundColor(getResources().getColor(R.color.colorLayout100));
                     fabButton.hideFloatingActionButton();
