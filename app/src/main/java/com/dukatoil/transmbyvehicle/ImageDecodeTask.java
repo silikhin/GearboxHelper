cce83a45994228f045f107f601a1c355e6103bf7
Borys Silikhin <silihin@gmail.com>
2017-03-24 09:17:43 +0200

    wrote ImageDecodeTask (not finished)

---
+++ app/src/main/java/com/dukatoil/transmbyvehicle/ImageDecodeTask.java
@@ -0,0 +1,15 @@
+package com.dukatoil.transmbyvehicle;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by B_Silikhin on 024 24.03.17.
+ */
+
+public class ImageDecodeTask extends AsyncTask <Integer, Void, Void> {
+    @Override
+    protected Void doInBackground(Integer... integers) {
+
+        return null;
+    }
+}
