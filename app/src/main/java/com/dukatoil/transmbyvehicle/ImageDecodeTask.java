d3d16ca83ffdaab6c05ad4c119460da4de6f38b7
Borys Silikhin <silihin@gmail.com>
2017-03-26 10:55:42 +0300

    finished correctly ImageDecodeTask class

--- app/src/main/java/com/dukatoil/transmbyvehicle/ImageDecodeTask.java
+++ app/src/main/java/com/dukatoil/transmbyvehicle/ImageDecodeTask.java
@@ -1,15 +1,32 @@
 package com.dukatoil.transmbyvehicle;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 
 /**
  * Created by B_Silikhin on 024 24.03.17.
  */
 
-public class ImageDecodeTask extends AsyncTask <Integer, Void, Void> {
-    @Override
-    protected Void doInBackground(Integer... integers) {
+public class ImageDecodeTask extends AsyncTask <Integer, Void, Bitmap> {
+    Context context;
+    Resources res;
+    Bitmap decodedImg;
 
-        return null;
+    public ImageDecodeTask(Context context) {
+        this.context = context;
+        res = context.getResources();
+    }
+
+    @Override
+    protected Bitmap doInBackground(Integer... integers) {
+        decodedImg = ImageDecoding.decodeSampledBitmapFromResource(res, integers[0]);
+        return decodedImg;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
     }
 }
