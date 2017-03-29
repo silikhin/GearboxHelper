027e403df3b4e5d73e46561262d1ef90a3ebcc4a
Borys Silikhin <silihin@gmail.com>
2017-03-29 10:00:20 +0300

    added faq activity

---
+++ app/src/main/java/com/dukatoil/transmbyvehicle/ActivityFaq.java
@@ -0,0 +1,17 @@
+package com.dukatoil.transmbyvehicle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ActivityFaq extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+    }
+
+}
