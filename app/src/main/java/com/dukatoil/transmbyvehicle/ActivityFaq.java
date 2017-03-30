626fabc4b40b2e5c494b5c53690728aa1f2e6afd
Borys Silikhin <silihin@gmail.com>
2017-03-30 08:52:41 +0300

    finished faq activity

--- app/src/main/java/com/dukatoil/transmbyvehicle/ActivityFaq.java
+++ app/src/main/java/com/dukatoil/transmbyvehicle/ActivityFaq.java
@@ -2,16 +2,25 @@ package com.dukatoil.transmbyvehicle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class ActivityFaq extends AppCompatActivity {
 
+    String[] questions, answers;
+    ListView lvFAQ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
 
+        questions = getResources().getStringArray(R.array.questions);
+        answers = getResources().getStringArray(R.array.answers);
+        lvFAQ = (ListView) findViewById(R.id.lvFAQ);
+
+        ArrayAdapterInfo adapterInfo = new ArrayAdapterInfo(this, R.layout.item_activity_transmission,
+                questions, answers);
+        lvFAQ.setAdapter(adapterInfo);
     }
 
 }
